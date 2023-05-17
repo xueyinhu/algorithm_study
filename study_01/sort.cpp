dd06155a05ace6a93a9e8743b2c4a829acd4b00f
xueyinhu <13453894389@163.com>
2023-05-17 23:52:30 +0800

    sort

--- study_01/sort.cpp
+++ study_01/sort.cpp
@@ -5,6 +5,12 @@ void arr_to_string(int arr[], int len) {
     for (int i = 0; i < len; i++) { cout << arr[i] << " "; }
 }
 
+/** 选择排序
+ * 时间复杂度：N^2/2 + N
+ *  缺点：运行时间与初始输入数组中排列无关
+ *  优点：只交换 N 次，数据移动是最小的
+ * 空间复杂度：O(1)
+ */
 void select_sort(int arr[], int len) {
     for (int i = 0; i < len - 1; i++) {
         int min_index = i, min_value = arr[i];
@@ -19,6 +25,15 @@ void select_sort(int arr[], int len) {
     }
 }
 
+/** 插入排序
+ * 时间复杂度：平均 N^2/4 + N^2/4 
+ *  最差 N^2/2 + N^2/2 
+ *  最好 N-1 + 0
+ *  优点：对于部分有序的输入数组，消耗时间是线性的，在这种情况下，其速度可能远高于其他排序算法
+ *  缺点：对于大规模乱序数组，表现很差
+ * 空间复杂度：O(1)
+ * 优化：在内循环中将较大的元素右移，而不是每次交换两个元素
+ */
 void insert_sort(int arr[], int len) {
     int value, t;
     for (int i = 1; i < len; i++) {
@@ -29,6 +44,19 @@ void insert_sort(int arr[], int len) {
     }
 }
 
+/** 希尔排序 基于插排，交换不相邻的元素来对输入数组的局部进行排序，然后再使用插排
+ * 时间复杂度：
+ */
+void shell_sort(int arr[], int len) {
+}
+
+/** 归并排序
+ * 
+ */
+
+/** 快速排序
+ * 
+ */
 void quick_sort(int left, int right, int arr[]) {
     if (left >= right) return;
     int i = left, j = right, base = arr[left], temp;
